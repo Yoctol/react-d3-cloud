4d1e19d74547fb108ec12ed4e33e265d8b4a3c36
cph <stegben.benjamin@gmail.com>
2016-11-23 20:54:50 +0800

    implement better structure of demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,14 +4,29 @@ import WordCloud from 'react-d3-cloud';
 
 import data from '../../hello.json';
 
-const fontSizeMapper = word => Math.log2(word.value) * 5;
-const rotate = word => word.value % 360;
+const fontSizeMapper = word => word.value / 20;
+const rotate = word => (word.value % 90) - 45;
+
+class WordCloudDemo extends React.Component {
+  render() {
+    const newData = data.map(item => ({
+      text: item.text,
+      value: Math.random() * 1000,
+    }));
+    return (
+      <WordCloud
+        width={1000}
+        height={750}
+        data={newData}
+        fontSizeMapper={fontSizeMapper}
+        rotate={rotate}
+        padding={2}
+      />
+    );
+  }
+}
 
 render(
-  <WordCloud
-    data={data}
-    fontSizeMapper={fontSizeMapper}
-    rotate={rotate}
-  />,
+  <WordCloudDemo />,
   document.getElementById('root')
 );
