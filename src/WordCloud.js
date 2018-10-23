01a22531101c5ba8a2bcf4cd0db313142ee127fb
chentsulin <chentsulin@gmail.com>
2018-10-23 23:25:55 +0800

    update dependencies

--- src/WordCloud.js
+++ src/WordCloud.js
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactFauxDom from 'react-faux-dom';
-import { select } from 'd3-selection';
-import { scaleOrdinal, schemeCategory10 } from 'd3-scale';
 import cloud from 'd3-cloud';
+import { Component } from 'react';
+import { scaleOrdinal, schemeCategory10 } from 'd3-scale';
+import { select } from 'd3-selection';
 
-import {
-  defaultFontSizeMapper,
-} from './defaultMappers';
-
+import { defaultFontSizeMapper } from './defaultMappers';
 
 const fill = scaleOrdinal(schemeCategory10);
 
@@ -19,27 +16,20 @@ const defaultClickEvent = word => {
 
 class WordCloud extends Component {
   static propTypes = {
-    data: PropTypes.arrayOf(PropTypes.shape({
-      text: PropTypes.string.isRequired,
-      value: PropTypes.number.isRequired,
-    })).isRequired,
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        text: PropTypes.string.isRequired,
+        value: PropTypes.number.isRequired,
+      })
+    ).isRequired,
     width: PropTypes.number,
     height: PropTypes.number,
-    padding: PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.func,
-    ]),
-    font: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.func,
-    ]),
+    padding: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
+    font: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
     fontSizeMapper: PropTypes.func,
     onWordClick: PropTypes.func,
-    rotate: PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.func,
-    ]),
-  }
+    rotate: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
+  };
 
   static defaultProps = {
     width: 700,
@@ -48,21 +38,30 @@ class WordCloud extends Component {
     font: 'serif',
     fontSizeMapper: defaultFontSizeMapper,
     rotate: 0,
-    onWordClick: defaultClickEvent
-  }
+    onWordClick: defaultClickEvent,
+  };
 
   componentWillMount() {
     this.wordCloud = ReactFauxDom.createElement('div');
   }
 
   render() {
-    const { data, width, height, padding, font, fontSizeMapper, rotate, onWordClick } = this.props;
-    const wordCounts = data.map(
-      text => ({ ...text })
-    );
+    const {
+      data,
+      width,
+      height,
+      padding,
+      font,
+      fontSizeMapper,
+      rotate,
+      onWordClick,
+    } = this.props;
+    const wordCounts = data.map(text => ({ ...text }));
 
     // clear old words
-    select(this.wordCloud).selectAll('*').remove();
+    select(this.wordCloud)
+      .selectAll('*')
+      .remove();
 
     // render based on new data
     const layout = cloud()
@@ -78,7 +77,10 @@ class WordCloud extends Component {
           .attr('width', layout.size()[0])
           .attr('height', layout.size()[1])
           .append('g')
-          .attr('transform', `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`)
+          .attr(
+            'transform',
+            `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`
+          )
           .selectAll('text')
           .data(words)
           .enter()
@@ -87,9 +89,7 @@ class WordCloud extends Component {
           .style('font-family', font)
           .style('fill', (d, i) => fill(i))
           .attr('text-anchor', 'middle')
-          .attr('transform',
-            d => `translate(${[d.x, d.y]})rotate(${d.rotate})`
-          )
+          .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
           .text(d => d.text)
           .on('click', d => onWordClick(d));
       });
@@ -99,6 +99,5 @@ class WordCloud extends Component {
     return this.wordCloud.toReact();
   }
 }
-
 
 export default WordCloud;
--- src/__tests__/WordCloud.spec.js
+++ src/__tests__/WordCloud.spec.js
@@ -3,37 +3,29 @@ import renderer from 'react-test-renderer';
 
 import WordCloud from '../WordCloud';
 
-describe('index.js', () => {
-  const data = [
-    { text: 'Hey', value: 1000 },
-    { text: 'lol', value: 200 },
-    { text: 'cool', value: 1 },
-    { text: 'duck', value: 10 },
-  ];
+const data = [
+  { text: 'Hey', value: 100 },
+  { text: 'lol', value: 200 },
+  { text: 'cool', value: 1 },
+  { text: 'duck', value: 10 },
+];
 
-  it('should contain all words', () => {
-    const originalRandom = Math.random;
-    Math.random = jest.fn().mockReturnValue(0.5);
-    const component = renderer.create(
-      <WordCloud data={data} />
-    ).toJSON();
-    expect(component).toMatchSnapshot();
-    Math.random = originalRandom;
-  });
+it('should contain all words', () => {
+  const originalRandom = Math.random;
+  Math.random = jest.fn().mockReturnValue(0.5);
+  const component = renderer.create(<WordCloud data={data} />).toJSON();
+  expect(component).toMatchSnapshot();
+  Math.random = originalRandom;
+});
 
-  it('should call custom fontSizeMapper', () => {
-    const fontSizeMapper = jest.fn().mockReturnValue(10);
-    renderer.create(
-      <WordCloud data={data} fontSizeMapper={fontSizeMapper} />
-    );
-    expect(fontSizeMapper.mock.calls.length).toBe(data.length);
-  });
+it('should call custom fontSizeMapper', () => {
+  const fontSizeMapper = jest.fn().mockReturnValue(10);
+  renderer.create(<WordCloud data={data} fontSizeMapper={fontSizeMapper} />);
+  expect(fontSizeMapper.mock.calls.length).toBe(data.length);
+});
 
-  it('should call custom rotater', () => {
-    const rotate = jest.fn().mockReturnValue(1);
-    renderer.create(
-      <WordCloud data={data} rotate={rotate} />
-    );
-    expect(rotate.mock.calls.length).toBe(data.length);
-  });
+it('should call custom rotater', () => {
+  const rotate = jest.fn().mockReturnValue(1);
+  renderer.create(<WordCloud data={data} rotate={rotate} />);
+  expect(rotate.mock.calls.length).toBe(data.length);
 });
