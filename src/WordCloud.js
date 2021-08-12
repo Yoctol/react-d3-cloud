8f02b8edda6c9c7cfe2d8173e1e60b1c67425fa8
chentsulin <chentsulin@gmail.com>
2021-08-13 01:27:44 +0800

    use react hooks

--- src/WordCloud.js
+++ src/WordCloud.js
@@ -1,84 +1,83 @@
 import PropTypes from 'prop-types';
 import ReactFauxDom from 'react-faux-dom';
 import cloud from 'd3-cloud';
-import { Component } from 'react';
 import { scaleOrdinal } from 'd3-scale';
 import { schemeCategory10 } from 'd3-scale-chromatic';
 import { select } from 'd3-selection';
+import { useRef } from 'react';
 
 import { defaultFontSizeMapper } from './defaultMappers';
 
 const fill = scaleOrdinal(schemeCategory10);
 
-class WordCloud extends Component {
-  componentWillMount() {
-    this.wordCloud = ReactFauxDom.createElement('div');
+function WordCloud(props) {
+  const elementRef = useRef();
+
+  if (!elementRef.current) {
+    elementRef.current = ReactFauxDom.createElement('div');
   }
 
-  render() {
-    const {
-      data,
-      width,
-      height,
-      padding,
-      font,
-      fontSizeMapper,
-      rotate,
-      onWordClick,
-      onWordMouseOver,
-      onWordMouseOut,
-    } = this.props;
+  const el = elementRef.current;
 
-    // clear old words
-    select(this.wordCloud).selectAll('*').remove();
+  const {
+    data,
+    width,
+    height,
+    padding,
+    font,
+    fontSizeMapper,
+    rotate,
+    onWordClick,
+    onWordMouseOver,
+    onWordMouseOut,
+  } = props;
 
-    // render based on new data
-    const layout = cloud()
-      .size([width, height])
-      .font(font)
-      .words(data)
-      .padding(padding)
-      .rotate(rotate)
-      .fontSize(fontSizeMapper)
-      .on('end', (words) => {
-        const texts = select(this.wordCloud)
-          .append('svg')
-          .attr('width', layout.size()[0])
-          .attr('height', layout.size()[1])
-          .append('g')
-          .attr(
-            'transform',
-            `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`
-          )
-          .selectAll('text')
-          .data(words)
-          .enter()
-          .append('text')
-          .style('font-size', (d) => `${d.size}px`)
-          .style('font-family', font)
-          .style('fill', (d, i) => fill(i))
-          .attr('text-anchor', 'middle')
-          .attr(
-            'transform',
-            (d) => `translate(${[d.x, d.y]})rotate(${d.rotate})`
-          )
-          .text((d) => d.text);
+  // clear old words
+  select(el).selectAll('*').remove();
 
-        if (onWordClick) {
-          texts.on('click', onWordClick);
-        }
-        if (onWordMouseOver) {
-          texts.on('mouseover', onWordMouseOver);
-        }
-        if (onWordMouseOut) {
-          texts.on('mouseout', onWordMouseOut);
-        }
-      });
+  // render based on new data
+  const layout = cloud()
+    .size([width, height])
+    .font(font)
+    .words(data)
+    .padding(padding)
+    .rotate(rotate)
+    .fontSize(fontSizeMapper)
+    .on('end', (words) => {
+      const texts = select(el)
+        .append('svg')
+        .attr('width', layout.size()[0])
+        .attr('height', layout.size()[1])
+        .append('g')
+        .attr(
+          'transform',
+          `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`
+        )
+        .selectAll('text')
+        .data(words)
+        .enter()
+        .append('text')
+        .style('font-size', (d) => `${d.size}px`)
+        .style('font-family', font)
+        .style('fill', (d, i) => fill(i))
+        .attr('text-anchor', 'middle')
+        .attr('transform', (d) => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+        .text((d) => d.text);
 
-    layout.start();
+      if (onWordClick) {
+        texts.on('click', onWordClick);
+      }
+      if (onWordMouseOver) {
+        texts.on('mouseover', onWordMouseOver);
+      }
+      if (onWordMouseOut) {
+        texts.on('mouseout', onWordMouseOut);
+      }
+    });
 
-    return this.wordCloud.toReact();
-  }
+  layout.start();
+
+  return el.toReact();
 }
 
 WordCloud.propTypes = {
