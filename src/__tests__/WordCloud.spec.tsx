b6c0eeffc4f65eb6fe4bdcfce54cc5baef30b029
chentsulin <chentsulin@gmail.com>
2021-10-03 02:11:24 +0800

    chore: merge tests into one file

--- src/__tests__/WordCloud.spec.tsx
+++ src/__tests__/WordCloud.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import WordCloud from '../WordCloud';
 
@@ -20,4 +21,22 @@ it('should call custom rotate', () => {
   const rotate = jest.fn().mockReturnValue(1);
   renderer.create(<WordCloud data={data} rotate={rotate} />);
   expect(rotate).toHaveBeenCalledTimes(data.length);
+});
+
+it('should render correct number of words', () => {
+  const component = shallow(<WordCloud data={data} />);
+  expect(component.find('g').children().length).toBe(4);
+});
+
+it('should render correct number of words after re-render', () => {
+  const component = shallow(<WordCloud data={data} />);
+  const newData = [
+    { text: 'Hey', value: 101 },
+    { text: 'lol', value: 199 },
+    { text: 'cool', value: 2 },
+    { text: 'duck', value: 30 },
+    { text: 'crazy', value: 60 },
+  ];
+  component.setProps({ data: newData });
+  expect(component.find('g').children().length).toBe(5);
 });
